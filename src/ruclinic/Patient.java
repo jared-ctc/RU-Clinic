cf93ac3b5e88532edd006c2456600aaa1bbc4808
jared-ctc <jaredcordova2001@gmail.com>
2024-09-29 15:35:29 -0400

    Patient class

--- src/ruclinic/Patient.java
+++ src/ruclinic/Patient.java
@@ -1,4 +1,25 @@
 package ruclinic;
 
-public class Patient {
+public class Patient implements Comparable<Patient> {
+    private Profile profile;
+    private Visit visits;
+
+    public int charge()
+    {
+        Visit ptr = this.visits;
+        int totalCharge = 0;
+
+        while (ptr != null)
+        {
+            ptr = ptr.getNext();
+        }
+
+        return totalCharge;
+    }
+
+    @Override
+    int compareTo(Patient patient)
+    {
+
+    }
 }
