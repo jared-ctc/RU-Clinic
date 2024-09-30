dab1087e03d70c5d2fa602db696c01994f683027
jared-ctc <jaredcordova2001@gmail.com>
2024-09-30 08:46:49 -0400

    Created the MedicalRecord linear data structure to hold a list of Patient objects, along with the add() method to add new Patient entries

--- src/ruclinic/MedicalRecord.java
+++ src/ruclinic/MedicalRecord.java
@@ -1,4 +1,37 @@
 package ruclinic;
 
-public class MedicalRecord {
+public class MedicalRecord
+{
+    private static final int INITIAL_CAPACITY = 4;
+    private static final int RESIZE_FACTOR = 2;
+
+    private Patient[] patients;
+    private int size; // number of patient objects in array
+
+    public MedicalRecord()
+    {
+        patients = new Patient[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void add(Patient patient)
+    {
+        if (size > patients.length)
+        {
+            resize();
+        }
+        patients[size++] = patient;
+    }
+
+    public void resize()
+    {
+        Patient[] newPatients = new Patient[patients.length * RESIZE_FACTOR];
+
+        for (int i = 0; i < patients.length; i++)
+        {
+            newPatients[i] = patients[i];
+        }
+
+        patients = newPatients;
+    }
 }
